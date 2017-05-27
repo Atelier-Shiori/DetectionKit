12a6a2b7f75e3f1d644cae43f07e821cb0c394fd
桐間紗路 <chikorita157@chikorita157.com>
2017-05-27 11:31:46 -0700

    Initial Commit

---
+++ DetectionKit/DetectionKit.h
@@ -0,0 +1,19 @@
+//
+//  DetectionKit.h
+//  DetectionKit
+//
+//  Created by 桐間紗路 on 2017/05/27.
+//  Copyright © 2017 Atelier Shiori. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for DetectionKit.
+FOUNDATION_EXPORT double DetectionKitVersionNumber;
+
+//! Project version string for DetectionKit.
+FOUNDATION_EXPORT const unsigned char DetectionKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DetectionKit/PublicHeader.h>
+
+
