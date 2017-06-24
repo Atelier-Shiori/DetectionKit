7cae8e398235ff9bf1be83284d9f576eafedb3eb
桐間紗路 <kirimasharo@kirikansharos-Mac-Pro.local>
2017-06-24 18:28:02 -0400

    Update detectstream

--- build/DetectionKit.build/Release/DetectionKit.build/Objects-normal/x86_64/DetectionKit_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/kirimasharo/Desktop/DetectionKit/build/DetectionKit.build/Release/DetectionKit.build/DerivedSources/DetectionKit_vers.c
--- build/DetectionKit.build/Release/DetectionKit.build/Objects-normal/x86_64/Recognition.d
+++
@@ -1,7 +0,0 @@
-dependencies: \
-  /Users/kirimasharo/Desktop/DetectionKit/DetectionKit/Recognition.m \
-  /Users/kirimasharo/Desktop/DetectionKit/DetectionKit/Recognition.h \
-  /Users/kirimasharo/Desktop/DetectionKit/DetectionKit/Frameworks/CocoaOniguruma.framework/Headers/OnigRegexp.h \
-  /Users/kirimasharo/Desktop/DetectionKit/DetectionKit/Frameworks/CocoaOniguruma.framework/Headers/oniguruma.h \
-  /Users/kirimasharo/Desktop/DetectionKit/DetectionKit/Frameworks/CocoaOniguruma.framework/Headers/OnigRegexpUtility.h \
-  /Users/kirimasharo/Desktop/DetectionKit/DetectionKit/Frameworks/anitomy-osx.framework/Headers/anitomy-objc-wrapper.h
